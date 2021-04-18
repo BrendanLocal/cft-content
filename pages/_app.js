bfb578b20fac75c04df226dc10d6081016721141
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-04-18 20:57:08 -0300

    Add initial TINA CMS Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react'
+import App from 'next/app'
+import { TinaProvider, TinaCMS } from 'tinacms'
+
+class MyApp extends App {
+  constructor() {
+    super()
+
+    // Instantiate the cms
+    this.cms = new TinaCMS({ enabled: true, sidebar: true })
+  }
+  render() {
+    const { Component, pageProps } = this.props
+
+    // Wrap the TinaProvider around all page components
+    return (
+      <TinaProvider cms={this.cms}>
+        <Component {...pageProps} />
+      </TinaProvider>
+    )
+  }
 }
 
 export default MyApp
