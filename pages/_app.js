92dec696833187756240a945112701725dbed842
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-04-18 21:08:58 -0300

    Added CMS sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,28 @@ import '../styles/globals.css'
 import React from 'react'
 import App from 'next/app'
 import { TinaProvider, TinaCMS } from 'tinacms'
+import { GitClient, GitMediaStore } from '@tinacms/git-client'
 
 class MyApp extends App {
   constructor() {
     super()
 
-    // Instantiate the cms
-    this.cms = new TinaCMS({ enabled: true, sidebar: true })
+    const client = new GitClient('/___tina')
+
+    this.cms = new TinaCMS({
+      enabled: true,
+      apis: {
+        git: client,
+      },
+      media: new GitMediaStore(client),
+      sidebar: {
+        position: 'overlay',
+      },
+    })
   }
+
   render() {
     const { Component, pageProps } = this.props
-
-    // Wrap the TinaProvider around all page components
     return (
       <TinaProvider cms={this.cms}>
         <Component {...pageProps} />
