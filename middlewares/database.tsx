d12ca299019ad8602527d0177ae7c6ea5b8e7381
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-14 20:53:49 -0300

    Adjust for GlobalAny

--- middlewares/database.tsx
+++ middlewares/database.tsx
@@ -1,12 +1,12 @@
 import { MongoClient } from 'mongodb';
-
+const globalAny:any = global;
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentiatlly
  * during API Route usage.
  * https://github.com/vercel/next.js/pull/17666
  */
-global.mongo = global.mongo || {};
+globalAny.mongo = globalAny.mongo || {};
 
 let indexesCreated = false;
 export async function createIndexes(db) {
@@ -20,15 +20,15 @@ export async function createIndexes(db) {
 }
 
 export default async function database(req, res, next) {
-  if (!global.mongo.client) {
-    global.mongo.client = new MongoClient(process.env.MONGODB_URI, {
+  if (!globalAny.mongo.client) {
+    globalAny.mongo.client = new MongoClient(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    await global.mongo.client.connect();
+    await globalAny.mongo.client.connect();
   }
-  req.dbClient = global.mongo.client;
-  req.db = global.mongo.client.db(process.env.DB_NAME);
+  req.dbClient = globalAny.mongo.client;
+  req.db = globalAny.mongo.client.db(process.env.DB_NAME);
   if (!indexesCreated) await createIndexes(req.db);
   return next();
 }
