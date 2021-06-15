8de8442dbb7bee05b017862a75ffc9db600995b6
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 18:40:31 -0300

    testing var send

--- pages/portal-user.js
+++ pages/portal-user.js
@@ -29,6 +29,8 @@ export default function Portal({ file }) {
   
 
 
+  var location = [47.185414, -66.314062];
+
 
 return (
 
@@ -83,7 +85,7 @@ return (
             <Col className="col-lg-9 p-5">
             <TabPanel>
               <h2 className="panelHead text-center text-grey">My Smart Forest<sup>™</sup></h2>
-              <Map location={[user.latitude, user.longitude]}/>
+              <Map location={user ? user.longitude : '0,0'}/>
               <Row className="borderGrid pt-3">
                 <Col>
                 <Row>
