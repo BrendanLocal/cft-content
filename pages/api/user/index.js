be84cc60dea7f963bfe7e9ec502ed365d4d3b3f1
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 06:08:58 -0300

    Remove cloudinary

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,6 +1,5 @@
 import nc from 'next-connect';
 import multer from 'multer';
-import { v2 as cloudinary } from 'cloudinary';
 import { all } from '../../../middlewares/index';
 import { updateUserById } from '../../../db/index';
 import { extractUser } from '../../../lib/api-helpers';
@@ -8,18 +7,6 @@ import { extractUser } from '../../../lib/api-helpers';
 const upload = multer({ dest: '/tmp' });
 const handler = nc();
 
-/* eslint-disable camelcase */
-const {
-  hostname: cloud_name,
-  username: api_key,
-  password: api_secret,
-} = new URL(process.env.CLOUDINARY_URL);
-
-cloudinary.config({
-  cloud_name,
-  api_key,
-  api_secret,
-});
 
 handler.use(all);
 
@@ -35,21 +22,9 @@ handler.patch(upload.single('profilePicture'), async (req, res) => {
     req.status(401).end();
     return;
   }
-  let profilePicture;
-  if (req.file) {
-    const image = await cloudinary.uploader.upload(req.file.path, {
-      width: 512,
-      height: 512,
-      crop: 'fill',
-    });
-    profilePicture = image.secure_url;
-  }
-  const { name, bio } = req.body;
-
+  
   const user = await updateUserById(req.db, req.user._id, {
-    ...(name && { name }),
-    ...(typeof bio === 'string' && { bio }),
-    ...(profilePicture && { profilePicture }),
+    ...(name && { name })
   });
 
   res.json({ user: extractUser(user) });
