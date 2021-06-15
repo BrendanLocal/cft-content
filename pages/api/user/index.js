6c5593a8dfd209d21ddc27bc4b1f22867a4752bf
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 15:17:04 -0300

    Fix index

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -17,7 +17,7 @@ handler.get(async (req, res) => {
   res.json({ user: u });
 });
 
-handler, async (req, res) => {
+handler.patch(upload.single('profilePicture'), async (req, res) => {
   if (!req.user) {
     req.status(401).end();
     return;
