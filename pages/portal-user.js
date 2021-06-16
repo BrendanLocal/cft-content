6eecd9c5a2812e21ab2a48099553ff4604378dc9
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 22:47:34 -0300

    reduce the effect

--- pages/portal-user.js
+++ pages/portal-user.js
@@ -25,22 +25,7 @@ export default function Portal({ file }) {
   const [location, setLocation] = useState([47.185414, -66.314062]);
   
   useEffect(() => {
-    console.log('useeffect');
-    const fetchLocation = async () => {
-      console.log('async');
-      await useCurrentUser().then((response) => {
-        console.log(response);
-        console.log(response.longitude);
-        setLocation([response.latitude, response.longitude]);
-      }).catch((err) => 
-      console.log({ err })
-      );
-    };
-
-
-    fetchLocation();
-
-
+setLocation([user.latitude, user.longitude]);
   }, []);
 
 
