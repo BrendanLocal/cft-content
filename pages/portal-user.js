7ab73fcd5a1b72b1b5b8d6316599494053eae683
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 23:21:41 -0300

    reverse lat long

--- pages/portal-user.js
+++ pages/portal-user.js
@@ -27,7 +27,7 @@ export default function Portal({ file }) {
   useEffect(() => {
     console.log('useEffect');
     console.log(Number(user.latitude), Number(user.longitude));
-setLocation([Number(user.latitude), Number(user.longitude)]);
+setLocation(Number(user.longitude), [Number(user.latitude)]);
   }, []);
 
 
