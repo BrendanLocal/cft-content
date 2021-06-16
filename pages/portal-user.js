4bed0b59ce1c004cfc3f3ab5fab51901c91d65a2
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-15 21:51:38 -0300

    Adding a useEffect function

--- pages/portal-user.js
+++ pages/portal-user.js
@@ -19,32 +19,16 @@ const Map = dynamic(() => import("../components/Map"), {
   ssr: false
 });
 
-const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/greggs.json?access_token=${process.env.MAPBOX_KEY}&bbox=-0.227654%2C51.464102%2C0.060737%2C51.553421&limit=10`;
-
-
 export default function Portal({ file }) {
-  const [locations, setLocations] = useState([]);
+
+  const [user] = useCurrentUser();
+
+  const [location, setLocation] = useState([]);
   
   useEffect(() => {
-    const fetchLocations = async () => {
-      await fetch(url).then((response) =>
-        response.text()).then((res) => JSON.parse(res))
-      .then((json) => {
-        setLocations(json.features);
-      }).catch((err) => console.log({ err }));
-    };
-    fetchLocations();
-  }, []);
- 
-  const [user] = useCurrentUser();
- 
-  if(user){
-    let lat = user.latitude;
-    let long = user.longitude;
-
-  console.log(lat)
-  console.log(long)
-}
+      setLocation([user.latitude, user.longitude]);
+    
+  });
   
   const [isUpdating, setIsUpdating] = useState(false);
   
