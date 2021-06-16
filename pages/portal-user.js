a74bc3f1ec7f2640a57722d958bd2aa99931100e
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-16 11:40:52 -0300

    Add editing tab

--- pages/portal-user.js
+++ pages/portal-user.js
@@ -21,13 +21,22 @@ const Map = dynamic(() => import("../components/portalMap"), {
 
 export default function Portal({ file }) {
 
-  const [user] = useCurrentUser();
+  const [user, { mutate }] = useCurrentUser();
+  const [isUpdating, setIsUpdating] = useState(false);
+  const nameRef = useRef();
+  const [msg, setMsg] = useState({ message: '', isError: false });
   const [location, setLocation] = useState([47.185414, -66.314062]);
   
   useEffect(() => {
+
+  
 setLocation([Number(user.longitude), Number(user.latitude)]);
 console.log(location);
   }, []);
+
+  useEffect(() => {
+    nameRef.current.value = user.name;
+  }, [user]);
 
 
   
@@ -37,10 +46,64 @@ console.log(location);
   const [msg, setMsg] = useState({ message: '', isError: false });
 
 
-  
+  /* for the settings editor */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isUpdating) return;
+    setIsUpdating(true);
+    const formData = new FormData();
+    formData.append('name', nameRef.current.value);
+    const res = await fetch('/api/user', {
+      method: 'PATCH',
+      body: formData,
+    });
+    if (res.status === 200) {
+      const userData = await res.json();
+      mutate({
+        user: {
+          ...user,
+          ...userData.user,
+        },
+      });
+      setMsg({ message: 'Profile updated' });
+    } else {
+      setMsg({ message: await res.text(), isError: true });
+    }
+    setIsUpdating(false);
+  };
 
+  const handleSubmitPasswordChange = async (e) => {
+    e.preventDefault();
+    const body = {
+      oldPassword: e.currentTarget.oldPassword.value,
+      newPassword: e.currentTarget.newPassword.value,
+    };
+    e.currentTarget.oldPassword.value = '';
+    e.currentTarget.newPassword.value = '';
 
+    const res = await fetch('/api/user/password', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
 
+    if (res.status === 200) {
+      setMsg({ message: 'Password updated' });
+    } else {
+      setMsg({ message: await res.text(), isError: true });
+    }
+  };
+
+  async function sendVerificationEmail() {
+    const res = await fetch('/api/user/email/verify', {
+      method: 'POST',
+    });
+    if (res.status === 200) {
+      setMsg({ message: 'An email has been sent to your mailbox' });
+    } else {
+      setMsg({ message: await res.text(), isError: true });
+    }
+  }
 
 return (
 
@@ -209,7 +272,52 @@ return (
             </TabPanel>
             <TabPanel>
               <h2 className="panelHead text-center text-grey">Edit My Information</h2>
-              
+              {msg.message ? <p style={{ color: msg.isError ? 'red' : '#0070f3', textAlign: 'center' }}>{msg.message}</p> : null}
+        <form onSubmit={handleSubmit}>
+          {!user.emailVerified ? (
+            <p>
+              Your email has not been verify.
+              {' '}
+              {/* eslint-disable-next-line */}
+                <a role="button" onClick={sendVerificationEmail}>
+                  Send verification email
+                </a>
+            </p>
+          ) : null}
+          <label htmlFor="name">
+            Name
+            <input
+              required
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name"
+              ref={nameRef}
+            />
+          </label>
+          <button disabled={isUpdating} type="submit">Save</button>
+        </form>
+        <form onSubmit={handleSubmitPasswordChange}>
+          <label htmlFor="oldpassword">
+            Old Password
+            <input
+              type="password"
+              name="oldPassword"
+              id="oldpassword"
+              required
+            />
+          </label>
+          <label htmlFor="newpassword">
+            New Password
+            <input
+              type="password"
+              name="newPassword"
+              id="newpassword"
+              required
+            />
+          </label>
+          <button type="submit">Change Password</button>
+        </form>
             </TabPanel>
 
             <TabPanel>
