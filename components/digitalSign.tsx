fb33eaa32893cf5e319e4baa32513921286ecd8d
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-06-23 12:42:47 -0300

    Adding file back in

---
+++ components/digitalSign.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, MouseEvent} from 'react';
+import { render } from 'react-dom';
+import Link from 'next/link'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const DigitalSign = ()=> {
+
+
+  return (
+
+<React.Fragment>
+    <Row>
+      <Col className="col-12 col-md-4">
+
+      </Col>
+      <Col className="col-12 col-md-8 signImagebuilder">
+        <div>
+          <img src="signbg_1.png"/>
+        </div>
+testi g
+      </Col>
+
+    </Row>
+    </React.Fragment>
+  )
+
+
+}
+
+export default DigitalSign
