f33741495dfd2eaf0f68aab21ca9fc4814ca18f3
phil84mac <phil@thinkingbig.net>
2021-07-27 17:58:11 -0300

    forgotten password page

--- pages/forgot-password/index.js
+++ pages/forgot-password/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const ForgetPasswordPage = () => {
   const [msg, setMsg] = useState({ message: '', isError: false });
@@ -26,22 +30,31 @@ const ForgetPasswordPage = () => {
 
   return (
     <>
+ 
       <Head>
-        <title>Forget password</title>
+        <title>Forgotten password</title>
       </Head>
-      <h2>Forget password</h2>
+      
+      <Container className="section-pad py-6 my-6">
+      <Row className="justify-content-center align-items-center py-6 my-6">
+        <Col className="col-11 col-md-10 col-lg-8 text-center text-white">
+        <h2 className="text-orange ">Forgotten password?</h2>
       {msg.message ? <p style={{ color: msg.isError ? 'red' : '#0070f3', textAlign: 'center' }}>{msg.message}</p> : null}
+      
       <form onSubmit={handleSubmit}>
         <p>Do not worry. Simply enter your email address below.</p>
-        <label htmlFor="email">
-          <input
+        <label className="col-6 me-3 no-border" htmlFor="email">
+          <input className="no-border"
             id="email"
             type="email"
             placeholder="Email"
           />
         </label>
-        <button type="submit">Submit</button>
+        <Button className="btn btn-green no-border" type="submit">Submit</Button>
       </form>
+        </Col>
+      </Row>
+    </Container>
     </>
   );
 };
