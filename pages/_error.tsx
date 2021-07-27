d0529aabcceea6ca9a7cf4587da6f9534db266df
BrendanLocal <82334693+BrendanLocal@users.noreply.github.com>
2021-07-27 10:38:48 -0300

    404 fixes

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
 import { useRouter } from "next/router"
 
-export default function Custom404() {
+export default function Custom404({ req }) {
   const router = useRouter()
-
-    router.replace("/home")
+  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
+    router.replace(baseUrl + "/home")
 
   return null
 }
