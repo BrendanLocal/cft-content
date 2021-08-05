f2f1aa6e40fb100efdffaf61353db1e78ed278d7
Grey <greymacisaac@gmail.com>
2021-08-05 13:30:21 -0300

    update accordion and add sub labels to all inputs on calculators but actually this time but not forgetting one

--- pages/net-negative-corp.tsx
+++ pages/net-negative-corp.tsx
@@ -158,6 +158,7 @@ if(negative > 0){
                   <label htmlFor="footprint">{editingdata.emissionsCarbonHeader}</label>
                   <br />
                   <input className="mb-4" value={footprint>0? footprint : ""} onChange={changeFootprint} name="type" type="number" min="0"  placeholder={editingdata.emissionsPlaceholder}/>
+                  <p className="x-small mb-3">={editingdata.emissionsPlaceholder}</p>
                   {editingdata.emissionsCarbon}<Link href="business-calculator"><a className="underline modal-btn">{editingdata.emissionsLink}</a></Link>
                 </Col>
               </Row>
