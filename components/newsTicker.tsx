1e586c5e57c2f2430f1acf331ffbf5cecfa3a058
Jordan Webster <jordan@thinkingbig.net>
2021-08-19 13:31:00 -0300

    Fixed issue with text in news ticker overlapping

--- components/newsTicker.tsx
+++ components/newsTicker.tsx
@@ -20,13 +20,13 @@ const newsItems = [
   },
   {
     key: "newsItem3",
-  label: "Carbon Calculators",
-  path: "/carbon-calculator"
+    label: "Carbon Calculators",
+    path: "/carbon-calculator"
   },
   {
     key: "newsItem4",
-  label: "A 'preview' of Earth's coming climate crisis",
-  path: "https://www.nbcnews.com/science/environment/heat-wave-2021-climate-scientists-warn-new-normal-rcna1664"
+    label: "A 'preview' of Earth's coming climate crisis",
+    path: "https://www.nbcnews.com/science/environment/heat-wave-2021-climate-scientists-warn-new-normal-rcna1664"
   },
   {
     key: "newsItem5",
@@ -35,21 +35,20 @@ const newsItems = [
   },
   {
     key: "newsItem6",
-  label: "Build a Smart Forest",
-  path: "/build-your-smart-forest"
+    label: "Build a Smart Forest",
+    path: "/build-your-smart-forest"
   },
   {
     key: "newsItem7",
-  label: "Can healthcare save the public from climate change?",
-  path: "https://www.corporateknights.com/channels/climate-and-carbon/after-covid-can-healthcare-save-the-public-from-climate-change-16273026/"
+    label: "Can healthcare save the public from climate change?",
+    path: "https://www.corporateknights.com/channels/climate-and-carbon/after-covid-can-healthcare-save-the-public-from-climate-change-16273026/"
   },
   {
     key: "newsItem8",
     label: "No Part of the Planet Will be Spared",
     path: "https://insideclimatenews.org/news/09082021/global-climate-panels-report-no-part-of-the-planet-will-be-spared-ipcc-science-cop-extremes/"
   }
-  ]
-
+];
 
 const NewsTicker = ()=> {
 
@@ -57,12 +56,21 @@ const NewsTicker = ()=> {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-return(
-  
- 
-<React.Fragment>
+  const [showTicker, setShowTicker] = useState(false);
+  const [moveTicker, setMoveTicker] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowTicker(true);
 
-<Modal show={show} className=" d-flex align-items-center" onHide={handleClose}>
+      setTimeout(() => {
+        setMoveTicker(true);
+      }, 500);
+    }, 250);
+  }, []);
+
+  return(
+    <React.Fragment>
+      <Modal show={show} className=" d-flex align-items-center" onHide={handleClose}>
         <Modal.Header className="d-none" closeButton>
           <Modal.Title className="d-none"></Modal.Title>
         </Modal.Header>
@@ -83,36 +91,35 @@ return(
         </Modal.Footer>
       </Modal>
 
-<div id="ticker" className="container-fluid">
-
-
-
-  <div className="row">
-    <div className="tickerHead v-10 col-3 col-sm-2 col-md-2 col-lg-1 px-4">
-      <span className="tickerTitle smallCaps">Latest News</span>
-    </div>
-    <div className="tickerMain col p-0">
-    <Ticker>
-      
-    {({ index }) => (
-              <><p className=" d-flex align-items-center v-10 justify-content-center">
-              {newsItems.map(function(item){
-                return (
-                  
-                  <a className={item.type? item.type+" newsItem" : "newsItem"} key={item.key} href={item.path? item.path : null} target="_blank" onClick={item.type? handleShow : null} >{item.label}</a>
-                  
-                )
-                })}
-                </p>
-              </>
-          )}
-    </Ticker>
-    </div>
-  </div>
-</div>
-</React.Fragment>
-)
+      <div id="ticker" className="container-fluid">
+        <div className="row">
+          <div className="tickerHead v-10 col-3 col-sm-2 col-md-2 col-lg-1 px-4">
+            <span className="tickerTitle smallCaps">Latest News</span>
+          </div>
+          <div className="tickerMain col p-0">
+          {
+            showTicker &&
+            <Ticker move={moveTicker}>
+              {({ index }) => (
+                <>
+                  <p className=" d-flex align-items-center v-10 justify-content-center">
+                  {
+                    newsItems.map(function(item){
+                      return (
+                        <a className={item.type? item.type+" newsItem" : "newsItem"} key={item.key} href={item.path? item.path : null} target="_blank" onClick={item.type? handleShow : null} >{item.label}</a>
+                      )
+                    })
+                  }
+                  </p>
+                </>
+              )}
+            </Ticker>
+          }
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
-
 
 export default NewsTicker
