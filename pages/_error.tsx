6e8392b86a3b0c4d5ab839ffebaae2c687312da1
Jordan Webster <jordan@thinkingbig.net>
2021-08-24 15:58:22 -0300

    Fixed redirect

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react"
 import { useRouter } from "next/router"
 
-
-
-export default function Custom404({ req }) {
-    const router = useRouter()
-    const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
-
-  useEffect(() => {
-    router.replace(baseUrl + "/home")
-
-  })
-  
+export function Custom404({ req }) {
   return null
 }
+
+Custom404.getInitialProps = ctx => {
+  // We check for ctx.res to make sure we're on the server.
+  if (ctx.res) {
+    ctx.res.writeHead(302, { Location: '/home' });
+    ctx.res.end();
+  }
+  return { };
+}
+
+export default Custom404;
