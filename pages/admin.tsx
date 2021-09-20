b2ed244db78f6ebe8eb181cdaa6a41d5725d701f
Brendan Henry <brendan@local.design>
2021-09-20 15:08:43 -0300

    CMS fixes

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,17 @@
 import React, { useState, useRef, useEffect, Component } from 'react';
+import { useRouter } from 'next/router'
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { GetStaticProps } from 'next'
 
-
+const Lang = () => {
+  var language ="en";
+    const router = useRouter();
+    if(router.query.lang){ 
+    const lan = JSON.stringify(router.query.lang);
+    language = JSON.parse(lan)
+    }
+    return (language)
+  }
 
 
 const SignupPage = () => {
@@ -13,3 +24,24 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
+export const getStaticProps: GetStaticProps = async function({preview, previewData,}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: 'content/home.json',
+      parse: parseJson,
+    })
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/home.json',
+        data: (await import('../content/home.json')).default,
+      },
+    },
+  }
+}
